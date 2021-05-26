ce14d48723dbdfeab3de01e838083cfad2c01b94
Shinichi Okada <okada.shin@gmail.com>
2021-05-26 15:53:54 +0900

    removed depends_on brew

--- trbf.rb
+++ trbf.rb
@@ -5,13 +5,12 @@ class Trbf < Formula
   sha256 "e2442de151a83bbacb9129411e0d1e8ba51dc8d68c16b6b2ded62d4794379395"
   license ""
 
-  depends_on "brew"
   depends_on "gh" 
 
   def install
     bin.install "trbf"
   end
-  
+
   test do
     system "false"
   end
