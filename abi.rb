3e6e402614adc1ffd94410864dc5acb95d51e112
Shinichi Okada <okada.shin@gmail.com>
2021-05-26 17:49:50 +0900

    file name changed to abi

--- abi.rb
+++ abi.rb
@@ -1,8 +1,8 @@
 class Trbf < Formula
-  desc "trbf stands for Transmit Brew Formula. It creates a Gist of your Homebrew formula or cask. Install Homebrew formula or cask from a Gist"
-  homepage "https://github.com/shinokada/trbf"
-  url "https://github.com/shinokada/trbf/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "e2442de151a83bbacb9129411e0d1e8ba51dc8d68c16b6b2ded62d4794379395"
+  desc "abi stands for Automate Brew Install. It creates a Gist of your Homebrew formula or cask. Install Homebrew formula or cask from a Gist"
+  homepage "https://github.com/shinokada/abi"
+  url "https://github.com/shinokada/abi/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "45dfc67bd4a5f217532a93c451abc71d4579dce0a80be416903d1b3ee2c778ce"
   license ""
 
   depends_on "gh" 
