ea176690d62e107efdafacf5223551e76e019864
Shinichi Okada <okada.shin@gmail.com>
2021-05-26 17:56:07 +0900

    Class name to Abi

--- abi.rb
+++ abi.rb
@@ -1,4 +1,4 @@
-class Trbf < Formula
+class Abi < Formula
   desc "abi stands for Automate Brew Install. It creates a Gist of your Homebrew formula or cask. Install Homebrew formula or cask from a Gist"
   homepage "https://github.com/shinokada/abi"
   url "https://github.com/shinokada/abi/archive/refs/tags/v0.0.4.tar.gz"
