7b2375e3056308234c0c6025a3559cc7fcc09966
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2026-01-21 17:17:53 +0100

    v0.2.0

--- abi.rb
+++ abi.rb
@@ -1,18 +1,20 @@
 class Abi < Formula
-  desc "abi stands for Automate Brew Install. It creates a Gist of your Homebrew formula or cask. Install Homebrew formula or cask from a Gist"
-  homepage "https://github.com/shinokada/abi"
-  url "https://github.com/shinokada/abi/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "56f8129cac6093b42ea1bade367546978153c82874bb788740eb2636b11c2d32"
+  desc "Homebrew installation automation."
+  homepage "https://abi.codewithshin.com"
+  url "https://github.com/shinokada/abi/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "c36980243c63c83992835c1f731599955540040b7e293964766ad298902ec559"
   license "MIT"
 
   depends_on "gh" 
   depends_on "bash"
+  depends_on "curl"
+  depends_on "jq"
 
   def install
     bin.install "abi"
   end
 
   test do
-    system "false"
+    system "#{bin}/abi", "--version"
   end
 end
